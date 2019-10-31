7b5a05619ea9547c230e3901af404d8bdfe246ad
tskyles <travis.skyles@gmail.com>
2019-10-31 09:03:39 -0700

    set up templating

--- js/app.js
+++ js/app.js
@@ -1,27 +1,35 @@
 'use strict';
 
 function Creature(creature) {
-  this.title = creature.title;
-  this.image_url = creature.image_url;
-  this.description = creature.description;
-  this.keyword = creature.keyword;
-  this.horns = creature.horns;
+  // this.title = creature.title;
+  // this.image_url = creature.image_url;
+  // this.description = creature.description;
+  // this.keyword = creature.keyword;
+  // this.horns = creature.horns;
+
+  for(let key in creature){
+    this[key] = creature[key];
+  }
 }
 
 Creature.allCreatures = [];
 Creature.keywords = [];
 
 Creature.prototype.render = function() {
-  let creatureClone = $('#photo-template, #photo-template-p2').clone();
-  let $creatureClone = $(creatureClone[0].content);
-  $creatureClone.find('h2').text(this.title);
-  $creatureClone.find('img').attr('src', this.image_url);
-  $creatureClone.find('p').text(this.description);
-  $creatureClone.find('section').attr('class', this.keyword);
-  $creatureClone.removeClass('clone');
-  $creatureClone.attr('class', this.title);
-  $creatureClone.appendTo('main');
-  Creature.keywords.push(this.keyword);
+  // let creatureClone = $('#photo-template, #photo-template-p2').clone();
+  // let $creatureClone = $(creatureClone[0].content);
+  // $creatureClone.find('h2').text(this.title);
+  // $creatureClone.find('img').attr('src', this.image_url);
+  // $creatureClone.find('p').text(this.description);
+  // $creatureClone.find('section').attr('class', this.keyword);
+  // $creatureClone.removeClass('clone');
+  // $creatureClone.attr('class', this.title);
+  // $creatureClone.appendTo('main');
+  // Creature.keywords.push(this.keyword);
+
+  let template = $('#photo-template').html();
+  let templateRender = Handlebars.compile(template);
+  return templateRender(this);
 }
 
 Creature.readJson = () => {
@@ -46,6 +54,10 @@ Creature.popList = () => {
     $('select').append($('<option></option>)').text(element).val(element));
   });
 }
+
+Creature.allCreatures.forEach(ourNewCreatureObject => {
+  $('#creatures').append(ourNewCreatureObject.toHtml);
+})
 
 $('select').on('change', function() {
   let $targetImage = $(this).val();
