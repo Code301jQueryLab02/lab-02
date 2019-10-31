eb8318a76717edb63718931a0080ee2a95d45cf9
Holly Davis <hollydavis541@gmail.com>
2019-10-31 10:17:12 -0700

    work on data changes based on page chosen

--- js/app.js
+++ js/app.js
@@ -24,8 +24,11 @@ Creature.prototype.render = function() {
   Creature.keywords.push(this.keyword);
 }
 
+let pageNumber = 1;
+
 Creature.readJson = () => {
-  $.get('../data/page-1.json') //replace 1 with template literal variable and then depending on which button you click it will set variable to one or two
+  // $('main').empty();
+  $.get(`../data/page-${pageNumber}.json`) //replace 1 with template literal variable and then depending on which button you click it will set variable to one or two
     .then(data => {
       data.forEach(item => {
         Creature.allCreatures.push(new Creature(item));
@@ -34,15 +37,16 @@ Creature.readJson = () => {
     .then(Creature.loadCreatures);
 };
 
-Creature.readJson2 = () => {
-  $.get('../data/page-2.json') //replace 1 with template literal variable and then depending on which button you click it will set variable to one or two
-    .then(data => {
-      data.forEach(item => {
-        Creature.allCreatures.push(new Creature(item));
-      });
-    })
-    .then(Creature.loadCreatures);
-};
+// Creature.readJson2 = () => {
+//   $.get('../data/page-2.json') //replace 1 with template literal variable and then depending on which button you click it will set variable to one or two
+//     .then(data => {
+//       data.forEach(item => {
+//         Creature.allCreatures.push(new Creature(item));
+//         console.log(Creature.allCreatures);
+//       });
+//     })
+//     .then(Creature.loadCreatures);
+// };
 
 Creature.loadCreatures = () => {
   Creature.allCreatures.forEach(creature => creature.render());
@@ -51,7 +55,7 @@ Creature.loadCreatures = () => {
 
 Creature.popList = () => {
   const set = new Set(Creature.keywords);
-  console.log(set);
+  // console.log(set);
   set.forEach( element => {
     $('select').append($('<option></option>)').text(element).val(element));
   });
@@ -71,10 +75,14 @@ $('nav a').on('click', function() {
   // $('#' + $whereToGo).Creature.readJson();
   
   if ($whereToGo === 'page1') {
+    pageNumber = 1;
     Creature.readJson();
   } else if ($whereToGo === 'page2') {
-    console.log('hi');
-    Creature.readJson2();
+    // console.log('hi');
+    pageNumber = 2;
+    // console.log(pageNumber);
+    Creature.readJson();
+    // console.log(Creature.allCreatures)
   }
   
   
@@ -88,7 +96,7 @@ $('nav a').on('click', function() {
 //   console.log('hi');
 // });
 
-// $(() => Creature.readJson())
+$(() => Creature.readJson())
 
 
 
